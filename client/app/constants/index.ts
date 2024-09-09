cc53a5efea4c2053fe0ccb47714a14f844618a5f
Ryan <ryanpvoong@gmail.com>
2024-09-09 12:05:56 -0400

    setup onboarding page layout

---
+++ client/app/constants/index.ts
@@ -0,0 +1,22 @@
+import heartHealth from "@/assets/images/heart-health.jpg";
+import meditation from "@/assets/images/meditation.jpg";
+
+export const images = {
+  heartHealth,
+  meditation,
+};
+
+export const onboarding = [
+  {
+    id: 1,
+    title: "Keep track of your health",
+    description: "Monitor your health, track your symptoms, and get personalized insights on your health.",
+    image: meditation
+  },
+  {
+    id: 2,
+    title: "Easily check your blood pressure",
+    description: "Check your blood pressure with ease by using your phone's front camera.",
+    image: heartHealth
+  },
+];
