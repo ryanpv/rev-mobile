f775bc959ceed63c231610cfba53f31ce2aba1a0
Ryan <ryanpvoong@gmail.com>
2024-09-09 10:58:59 -0400

    added reroute for onboarding screen. also testing git reinit

--- client/app/(auth)/welcome.tsx
+++ client/app/(auth)/welcome.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
+import { router } from "expo-router";
+import { View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const OnBoarding = () => {
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
-      <Text className="text-blue-500">Welcome to onboarding</Text>
+      <TouchableOpacity 
+        onPress={ () => {
+          router.replace("/(auth)/sign-up")
+        } }
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-blue-500 text-md font-JakartaBold">Welcome to onboarding</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
