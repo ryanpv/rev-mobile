eabd6391831f5aaaf49523ccf49047b0308d750f
Ryan <ryanpvoong@gmail.com>
2024-09-23 21:03:11 -0400

    creating config for server

---
+++ server/main/config.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class BaseConfig(BaseSettings):
+    ENV_STATE: Optional[str] = None
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
