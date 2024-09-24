47bc0386d6ce39b8cb4e661a8854ba85a17d263f
Ryan <ryanpvoong@gmail.com>
2024-09-23 22:44:14 -0400

    added more config. route setup for rppg

--- server/main/config.py
+++ server/main/config.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from typing import Optional
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -7,3 +8,25 @@ class BaseConfig(BaseSettings):
     ENV_STATE: Optional[str] = None
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+class GlobalConfig(BaseConfig):
+    # This is a global configuration for environment variables
+    DB_URL: Optional[str] = None
+
+
+class DevConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="DEV_", extra="ignore")
+
+
+class ProdConfig(GlobalConfig):
+    model_config = SettingsConfigDict(env_prefix="PROD_", extra="ignore")
+
+
+@lru_cache()
+def get_config(env_state: str):
+    configs = {"dev": DevConfig, "prod": ProdConfig}
+    return configs[env_state]()
+
+
+config = get_config(BaseConfig().ENV_STATE)
--- server/main/main.py
+++ server/main/main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+
+# Routes
+from main.routers.rPPG import router as rPPG_router
+
+app = FastAPI()
+
+app.include_router(rPPG_router)  # prefix="/rPPG" ???
+
 print("Main server file read")
