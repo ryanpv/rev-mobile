505aa354bf26196c3d000538b06cf90a1feba774
Ryan <ryanpvoong@gmail.com>
2024-09-25 01:23:37 -0400

    setup route to test out opencv functionality

--- server/main/main.py
+++ server/main/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # Routes
 from main.routers.rPPG import router as rPPG_router
 
 app = FastAPI()
+
+
+origins = ["http://192.168.2.43:3000", "http://localhost:8081", "http://localhost:8000"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(rPPG_router)  # prefix="/rPPG" ???
 
--- server/main/routers/rPPG.py
+++ server/main/routers/rPPG.py
@@ -1,3 +1,6 @@
+import threading
+
+import cv2
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -5,4 +8,40 @@ router = APIRouter()
 
 @router.get("/")
 async def root():
+    print("rPPG route reached.")
     return {"message": "rPPG route reached."}
+
+
+camera_active = False
+
+
+def access_camera():
+    global camera_active
+    cap = cv2.VideoCapture(0)
+
+    while camera_active:
+        ret, frame = cap.read()
+
+        if ret:
+            cv2.imshow("Camera", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+@router.get("/start_camera")
+def start_camera():
+    global camera_active
+    if not camera_active:
+        camera_active = True
+        threading.Thread(target=access_camera).start()
+    return {"message": "Camera started."}
+
+
+@router.get("/stop_camera")
+def stop_camera():
+    global camera_active
+    camera_active = False
+    return {"message": "Camera stopped."}
